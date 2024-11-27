2d9edd911a60146f713e7fa944b0004b4da3420f
ZalviaInasya <166111888+ZalviaInasya@users.noreply.github.com>
2024-11-28 06:46:12 +0700

    Update Transaksi.java

--- Transaksi.java
+++ Transaksi.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 
 public class Transaksi {
+    // Atribut customer dan daftar barang yang dibeli
     private Customer customer;
     private ArrayList<Barang> barang;
 
+    // Konstruktor Transaksi dengan parameter customer dan daftar barang
     public Transaksi(Customer customer, ArrayList<Barang> barang) {
         this.customer = customer;
         this.barang = barang;
@@ -13,10 +15,12 @@ public class Transaksi {
         return customer;
     }
 
+    // Getter untuk daftar barang
     public ArrayList<Barang> getBarang() {
         return barang;
     }
 
+    // Override metode toString untuk memberikan informasi transaksi
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
