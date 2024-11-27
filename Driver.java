db03d93dbff52b456ab50f579c67f75cea65bba4
Dwifitriawindy <167391058+Dwifitriawindy@users.noreply.github.com>
2024-11-28 00:56:14 +0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,9 +1,14 @@
+// Kelas Driver bersifat abstract, digunakan sebagai kelas dasar untuk pengelolaan menu admindriver dan customerdriver.
+// Tidak dapat diinstansiasi langsung, tetapi harus diturunkan oleh kelas konkret lainnya.
 public abstract class Driver {
     protected Akun akun;
 
-    public Driver(Akun akun) {
+    // Konstruktor untuk menginisialisasi atribut akun.
+    public Driver(Akun akun) { 
         this.akun = akun;
     }
 
+    // Method abstract untuk menangani menu.
+    // Harus diimplementasikan oleh kelas turunan untuk memberikan logika spesifik pada menu.
     public abstract void handleMenu();
 }
