20ee6ddd692fd54aea0790a51bfe9f54a65bd0e3
Dwifitriawindy <167391058+Dwifitriawindy@users.noreply.github.com>
2024-11-28 01:15:12 +0700

    Update Pembayaran.java

--- Pembayaran.java
+++ Pembayaran.java
@@ -1,3 +1,4 @@
+// Kelas abstrak ini merepresentasikan metode pembayaran secara umum.
 public abstract class Pembayaran {
     protected String id;
 
@@ -5,24 +6,29 @@ public abstract class Pembayaran {
         this.id = id;
     }
 
+    // Metode toString() untuk mengembalikan representasi string dari objek Pembayaran,
+    // termasuk nama kelas dan ID metode pembayaran.
     @Override
     public String toString() {
         return this.getClass().getSimpleName() + " - ID: " + id;
     }
 }
 
+// Kelas QRIS merupakan subclass dari Pembayaran, yang merepresentasikan metode pembayaran QRIS.
 class QRIS extends Pembayaran {
     public QRIS() {
         super("QRIS001");
     }
 }
 
+// Kelas Bank merupakan subclass dari Pembayaran, yang merepresentasikan metode pembayaran melalui bank.
 class Bank extends Pembayaran {
     public Bank() {
         super("BANK001");
     }
 }
 
+// Kelas COD merupakan subclass dari Pembayaran, yang merepresentasikan metode pembayaran tunai saat pengiriman.
 class COD extends Pembayaran {
     public COD() {
         super("COD001");
