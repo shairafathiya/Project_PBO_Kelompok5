53bc8d85278ad3c709b66e0c571495939e3e6b61
Dwifitriawindy <167391058+Dwifitriawindy@users.noreply.github.com>
2024-11-28 01:04:58 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,23 +1,30 @@
 import java.util.ArrayList;
 
+// Kelas Customer adalah subclass dari kelas Akun.
+// Kelas ini merepresentasikan akun dengan peran sebagai customer
 public class Customer extends Akun {
     private Keranjang keranjang;
     private ArrayList<Invoice> invoiceSelesai;
 
+    // Konstruktor Customer yang memanggil konstruktor superclass (Akun)
     public Customer(String id, String username, String password) {
         super(id, username, password);
-        this.keranjang = new Keranjang();
-        this.invoiceSelesai = new ArrayList<>();
+        this.keranjang = new Keranjang(); // Membuat objek Keranjang baru untuk pelanggan.
+        this.invoiceSelesai = new ArrayList<>(); // Inisialisasi ArrayList untuk invoice selesai.
     }
 
+    // Method ini digunakan untuk mengakses keranjang customer.
     public Keranjang getKeranjang() {
         return keranjang;
     }
 
+    // Method ini digunakan untuk mengakses daftar transaksi yang sudah selesai.
     public ArrayList<Invoice> getInvoiceSelesai() {
         return invoiceSelesai;
     }
 
+    // Implementasi method abstract menu() dari kelas Akun.
+    // Method ini digunakan untuk menampilkan daftar menu yang tersedia untuk pelanggan.
     @Override
     public void menu() {
         System.out.println("\nMenu Customer:");
