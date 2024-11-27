9a5e74e31f2b65e7b2989826486ad652f4ae89d4
Dwifitriawindy <167391058+Dwifitriawindy@users.noreply.github.com>
2024-11-28 00:58:15 +0700

    Update Akun.java

--- Akun.java
+++ Akun.java
@@ -1,8 +1,12 @@
+// Kelas Akun bersifat abstract, digunakan sebagai kelas dasar untuk berbagai jenis akun.
+// Tidak dapat diinstansiasi langsung dan harus diturunkan oleh kelas konkret.
 public abstract class Akun {
     protected String id;
     protected String username;
     protected String password;
 
+    // Konstruktor untuk menginisialisasi atribut id, username, dan password.
+    // Parameter yang diterima harus diisi saat membuat objek dari kelas turunan.
     public Akun(String id, String username, String password) {
         this.id = id;
         this.username = username;
@@ -21,5 +25,7 @@ public abstract class Akun {
         return password;
     }
 
+    // Method abstract menu yang harus diimplementasikan oleh kelas turunan.
+    // Berfungsi untuk menampilkan atau menangani logika menu berdasarkan jenis akun.
     public abstract void menu();
 }
