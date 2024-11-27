5a043e54ef27a49432c8e73de58bea0a95b5ab3e
Dwifitriawindy <167391058+Dwifitriawindy@users.noreply.github.com>
2024-11-28 01:07:39 +0700

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -2,15 +2,20 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+// Kelas Invoice merepresentasikan tagihan untuk transaksi tertentu.
+// Kelas ini menyimpan detail transaksi dan metode pembayaran yang digunakan.
 public class Invoice {
     private Transaksi transaksi;
     private Pembayaran pembayaran;
 
+    // Konstruktor kelas Invoice.
     public Invoice(Transaksi transaksi, Pembayaran pembayaran) {
         this.transaksi = transaksi;
         this.pembayaran = pembayaran;
     }
 
+    // Override method toString() untuk memberikan representasi string dari objek Invoice.
+    // Method ini juga menulis isi invoice ke dalam file "Invoice.txt".
     @Override
     public String toString() {
          try (BufferedWriter catat  = new BufferedWriter(new FileWriter("Invoice.txt"))) {
@@ -19,7 +24,7 @@ public class Invoice {
         } catch (IOException ex) {
         }
            
-
+        // Mengembalikan representasi string dari invoice untuk ditampilkan di konsol atau digunakan di tempat lain.
         return "Invoice:\n" + transaksi + "Metode Pembayaran: " + pembayaran;
     }
 }
